06d60c7daeb607539ae3e417c03405befe80dc63
hankstram <hstram@hotmail.com>
2021-10-02 10:24:32 -0400

    added java 8 features and modified pom.xml to support java 8 compiler features

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,9 +1,13 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     
     public Application() {
 		System.out.println ("Inside Application");
+		List<String> list = new ArrayList<String>();
     }
 
     // method main(): ALWAYS the APPLICATION entry point
